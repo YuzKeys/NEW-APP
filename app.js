66bad5097eac64b0cccfb8457f89ca686a24a24c
Larbi Youssef <larbiyoussef93@gmail.com>
2024-12-23 12:38:48 +0100

    edit

--- app.js
+++ app.js
@@ -9,12 +9,12 @@ btnLeft.addEventListener('click', function () {
     imgTurningCookie.src = 'images/Chuncky Chocolat CookieDetoure.png'
     infoContainer.style.backgroundColor = "white"
     cookyName.innerHTML = "Chuncky Chocolat"
-    // imageContainer.style.backgroundImage = "url('https://media1.giphy.com/"
+    imageContainer.style.backgroundImage = "url('https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExYnprbTQwN2Zud2dpNWlxaGh5MTlrNzg4bjQ1dXk0cWR6c2xqZzU1aiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3oEdv1GbekAakxXO8g/giphy.webp')"
 });
 
 btnRight.addEventListener('click', function () {
     imgTurningCookie.src = 'images/Caramel Naughty.png'
     infoContainer.style.backgroundColor = "white"
     cookyName.innerHTML = "Crunchy Caramel"
-    // imageContainer.style.backgroundImage = "url('https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExNmdjbTVhM25ydWdkZ3Rna2UwanptNWpma3diZ3ZtYWt4ZjAwMWhycyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/xTkcEYaBTfgoTUuO2c/giphy.webp"
+    imageContainer.style.backgroundImage = "url('https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExdXRxcmRrOGdwaG1hNWNhZDE1MWtiMzJvYjNmY3lwOXJtMzBwYWl1NiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/JlCWAFQLTnYx4Sly1c/giphy.webp')"
 })
